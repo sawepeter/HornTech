cb302d390e0ede7dca15d914b9a81046c16295ea
sawepeter <sawepeter6@gmail.com>
2018-12-12 19:17:13 +0300

    intial commit :tada:

--- app/src/main/java/com/example/devsawe/horntech/Model/HomeModel.java
+++ app/src/main/java/com/example/devsawe/horntech/Model/HomeModel.java
@@ -2,49 +2,10 @@ package com.example.devsawe.horntech.Model;
 
 public class HomeModel {
 
-    private int image;
-    private String name, route, book;
+    int Background;
+    String DriverName;
+    int profilePhoto;
+    int btnfollow;
+    int DriverContact;
 
-    public int getImage() {
-        return image;
-    }
-
-    public void setImage(int image) {
-        this.image = image;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRoute() {
-        return route;
-    }
-
-    public void setRoute(String route) {
-        this.route = route;
-    }
-
-    public String getBook() {
-        return book;
-    }
-
-    public void setBook(String book) {
-        this.book = book;
-    }
-
-    public HomeModel(int image, String name, String route, String book) {
-        this.image = image;
-        this.name = name;
-        this.route = route;
-        this.book = book;
-
-
-
-
-    }
 }
