797643a466f6cdafd3cdb9e56731a1777bdda86d
sawepeter <sawepeter6@gmail.com>
2018-12-12 11:30:03 +0300

    lets code :hammer:

--- app/src/main/java/com/example/devsawe/horntech/Home.java
+++ app/src/main/java/com/example/devsawe/horntech/Home.java
@@ -2,12 +2,26 @@ package com.example.devsawe.horntech;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
 
 public class Home extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        recyclerView = findViewById(R.id.rv);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        RecyclerView.LayoutManager rvLiLayoutManager = layoutManager;
+
+        recyclerView.setLayoutManager(rvLiLayoutManager);
+
+
     }
 }
