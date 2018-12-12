5c41cbb4c4f5bc23d9edba8efefe7987bb19f5c8
sawepeter <sawepeter6@gmail.com>
2018-12-12 16:03:44 +0300

    lets debug :bug:

--- app/src/main/java/com/example/devsawe/horntech/Model/HomeModel.java
+++ app/src/main/java/com/example/devsawe/horntech/Model/HomeModel.java
@@ -5,11 +5,45 @@ public class HomeModel {
     private int image;
     private String name, route, book;
 
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    public String getBook() {
+        return book;
+    }
+
+    public void setBook(String book) {
+        this.book = book;
+    }
+
     public HomeModel(int image, String name, String route, String book) {
         this.image = image;
         this.name = name;
         this.route = route;
         this.book = book;
+
+
 
 
     }
