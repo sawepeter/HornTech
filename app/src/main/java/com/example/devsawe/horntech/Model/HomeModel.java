dede4df90c69284e79a2bc4d8011639c256e7236
sawepeter <sawepeter6@gmail.com>
2018-12-12 16:03:07 +0300

    lets code :hammer:

--- app/src/main/java/com/example/devsawe/horntech/Model/HomeModel.java
+++ app/src/main/java/com/example/devsawe/horntech/Model/HomeModel.java
@@ -5,6 +5,12 @@ public class HomeModel {
     private int image;
     private String name, route, book;
 
+    public HomeModel(int image, String name, String route, String book) {
+        this.image = image;
+        this.name = name;
+        this.route = route;
+        this.book = book;
 
 
+    }
 }
