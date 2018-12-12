1c2358259ccf0f2ff479870bbab1f5ad9bb567f2
sawepeter <sawepeter6@gmail.com>
2018-12-12 11:51:10 +0300

    lets code :hammer:

--- app/src/main/java/com/example/devsawe/horntech/MainActivity.java
+++ app/src/main/java/com/example/devsawe/horntech/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.devsawe.horntech;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.devsawe.horntech.auth.SignIn;
+import com.example.devsawe.horntech.auth.SignUp;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnsignin,btnsignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnsignin = findViewById(R.id.btn_sign_in);
+        btnsignup = findViewById(R.id.btn_sign_up);
+
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signup = new Intent(MainActivity.this, SignUp.class);
+                startActivity(signup);
+            }
+        });
+        btnsignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signin = new Intent(MainActivity.this, SignIn.class);
+                startActivity(signin);
+            }
+        });
     }
 }
