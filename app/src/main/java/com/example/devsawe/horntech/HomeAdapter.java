7978fddbe3a13ead9607a8af72c4b3947bd0def6
sawepeter <sawepeter6@gmail.com>
2018-12-12 12:58:19 +0300

    lets code :hammer:

--- app/src/main/java/com/example/devsawe/horntech/HomeAdapter.java
+++ app/src/main/java/com/example/devsawe/horntech/HomeAdapter.java
@@ -1,14 +1,27 @@
 package com.example.devsawe.horntech;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
+
+    private Context mContext;
+    HomeAdapter(Context context){
+        mContext = context;
+    }
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+
+        View view = layoutInflater.inflate(R.layout.home_item, viewGroup, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+
         return null;
     }
 
