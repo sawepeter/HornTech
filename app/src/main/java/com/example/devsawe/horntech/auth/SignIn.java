5e14e31b166a42b8566f033a342b17eb2694543a
sawepeter <sawepeter6@gmail.com>
2018-12-12 17:12:23 +0300

    intial commit :tada:

--- app/src/main/java/com/example/devsawe/horntech/auth/SignIn.java
+++ app/src/main/java/com/example/devsawe/horntech/auth/SignIn.java
@@ -1,15 +1,32 @@
 package com.example.devsawe.horntech.auth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.devsawe.horntech.Home;
 import com.example.devsawe.horntech.R;
 
 public class SignIn extends AppCompatActivity {
+
+    Button btnsignin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        btnsignin = findViewById(R.id.btn_sign_in);
+
+        btnsignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignIn.this, Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
